42379f10694853c104731ea9a2d76314842043bc
danielhvs <danielhvs70@gmail.com>
2018-10-02 08:49:00 -0300

    basico

--- src/cljs/precos/core.cljs
+++ src/cljs/precos/core.cljs
@@ -2,13 +2,32 @@
     (:require [reagent.core :as reagent :refer [atom]]
               [secretary.core :as secretary :include-macros true]
               [accountant.core :as accountant]))
+(enable-console-print!)
+
+(println "oi mundo")
+
+;; -------------------------
+;; Estado
+(defonce moedas (atom {:peso {:nome "PES" :preco-cambio 0.33 :preco-loja 0 :carteira 0 :reais-carteira 0}}))
+
+;; -------------------------
+;; Componentes
+(defn input-valor []
+  [:input {:type "text" 
+           :on-change #("oi")}])
+ 
+(defn texto [] 
+  [:label "TESTE TEXTO" ])
 
 ;; -------------------------
 ;; Views
 
 (defn home-page []
-  [:div [:h2 "Welcome to precos"]
-   [:div [:a {:href "/about"} "go to about page"]]])
+  [:div
+   [:div [:label "Produto"] [input-valor]]
+   [:div [:label "Valor"] [input-valor]]
+   [:div [:button "Cadastrar"]]
+])
 
 (defn about-page []
   [:div [:h2 "About precos"]
