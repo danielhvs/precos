23f522d9e8c4520fd35ae4f28999659377f0a15f
danielhvs <danielhvs70@gmail.com>
2018-10-04 08:34:15 -0300

    formatacao double e correcao data para local-time

--- src/cljs/precos/core.cljs
+++ src/cljs/precos/core.cljs
@@ -1,8 +1,8 @@
 (ns precos.core
     (:require [reagent.core :as reagent :refer [atom]]
               [secretary.core :as secretary :include-macros true]
-              [cljs-time.core :as t]
               [cljs-time.format :as f]
+              [cljs-time.local :as l]
               [accountant.core :as accountant]))
 
 (enable-console-print!)
@@ -11,7 +11,7 @@
 ;; Estado
 (defonce produtos (atom []))
 (defonce cache-produto (atom ""))
-(defonce cache-valor (atom ""))
+(defonce cache-preco (atom ""))
 (defonce cache-local (atom ""))
 
 ;; -------------------------
@@ -19,13 +19,18 @@
 (defn formata [prefixo p sufixo]
   (str prefixo p sufixo))
 
-(defn aspas [p]
+(defn formata-aspas [p]
   (formata "'" p "'"))
-(defn reais [p]
-  (formata "R$" p ""))
+(defn formata-reais [p]
+  (formata "R$ " p ""))
+(defn formata-data [p]
+  (f/unparse (f/formatter "DD/MM/yyyy hh:mm:ss") p))
+
+(defn ->reais [p]
+  (double p))
 
 (defn cadastra [] 
-  (let [p {:produto @cache-produto :valor @cache-valor :data (t/now) :local @cache-local}]
+  (let [p {:produto @cache-produto :preco @cache-preco :data (l/local-now) :local @cache-local}]
     (do
       (swap! produtos conj p)
       )))
@@ -34,44 +39,47 @@
 ;; Componentes
 (defn input-element
   "An input element which updates its value on change"
-  [id name type value]
+  [id name type value f]
   [:input {:id id
            :name name
            :class "form-control"
            :type type
            :required ""
            :value @value
-           :on-change #(reset! value (-> % .-target .-value))}])
+           :on-change #(reset! value (f (-> % .-target .-value)))}])
+
+(defn colunas-tabela []
+  [:tr [:td "Produto"] [:td "Preco"] [:td "Data"] [:td "Local"]])
 
 (defn elemento [v]
   [:tr 
-   [:td (aspas (:produto v))] 
-   [:td (reais (:valor v))] 
-   [:td (f/unparse (f/formatter "DD/MM/yyyy hh:mm:ss") (:data v))] 
-   [:td (:local v)]])
+   [:td (formata-aspas (:produto v))] 
+   [:td (formata-reais (:preco v))] 
+   [:td (formata-data (:data v))] 
+   [:td (formata-aspas (:local v))]])
 
 (defn tabela []
   (let [visao (atom []) ]
     (fn []
       (doall
-        (reset! visao (filter #(= @cache-produto (:produto %)) @produtos))
-        [:div 
-         (when-let [v (first (sort-by :preco @visao))]
-           [:table  {:border 2}
-            [:caption "Mais barato"]
-            [:tr [:td "Produto"] [:td "Preco"] [:td "Data"] [:td "Local"]]
-            (elemento v)])
-         (when (not (empty? @visao))
-           [:table  {:border 2}
-            [:caption "Histórico"]
-            [:tr [:td "Produto"] [:td "Preco"] [:td "Data"] [:td "Local"]]
-            (for [v @visao]
-              (elemento v))])]))))
+       (reset! visao (filter #(= @cache-produto (:produto %)) @produtos))
+       [:div
+        (when-let [v (first (sort-by :preco @visao))]
+          [:table  {:border 2}
+           [:caption "Mais barato"]
+           (colunas-tabela)
+           (elemento v)])
+        (when (not (empty? @visao))
+          [:table  {:border 2}
+           [:caption "Histórico"]
+           (colunas-tabela)
+           (for [v @visao]
+             (elemento v))])]))))
 
 (defn debug []
   [:div [:label "DEBUG ABAIXO"] 
    [:div [:label (str "produto: " @cache-produto)]] 
-   [:div [:label (str "valor: " @cache-valor)]]
+   [:div [:label (str "preco: " @cache-preco)]]
    [:div [:label (str "produtos: " @produtos)]]
 ])
 
@@ -79,9 +87,9 @@
 ;; Views
 (defn home-page []
   [:div
-   [:div [:label "Produto"] (input-element "p" "p" "input" cache-produto) ]
-   [:div [:label "Valor"] (input-element "v" "v" "input" cache-valor)]
-   [:div [:label "Local"] (input-element "l" "l" "input" cache-local)]
+   [:div [:label "Produto"] (input-element "p" "p" "input" cache-produto identity) ]
+   [:div [:label "Preco"] (input-element "v" "v" "input" cache-preco ->reais)]
+   [:div [:label "Local"] (input-element "l" "l" "input" cache-local identity)]
    [:div
     [:input {:type :button :value "Cadastra" :on-click #(cadastra)}]
     (for [p (distinct (map :produto @produtos))]
