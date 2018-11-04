476c13646d731648b2386ceb06a6ae15ce3245a8
danielhvs <danielhvs70@gmail.com>
2018-11-04 14:47:00 -0200

    apontamento servidor correto

--- src/cljs/precos/core.cljs
+++ src/cljs/precos/core.cljs
@@ -106,8 +106,8 @@
 
 ;; -------------------------
 ;; Estado
-#_(defonce servidor "https://infinite-crag-89428.herokuapp.com/")
-(defonce servidor "http://localhost:3000/")
+(defonce servidor "https://infinite-crag-89428.herokuapp.com/")
+#_(defonce servidor "http://localhost:3000/")
 
 ;; -------------------------
 ;; Funcoes
