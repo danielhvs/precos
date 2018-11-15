7e52974c77903a615cba3c05f68d937bea14fcbd
danielhvs <danielhvs70@gmail.com>
2018-11-15 18:45:44 -0200

    precos-mobile

---
+++ precos-mobile/env/prod/env/android/main.cljs
@@ -0,0 +1,6 @@
+ (ns env.android.main
+  (:require [precos-mobile.android.core :as core]))
+
+ (core/init)
+
+
---
+++ precos-mobile/env/prod/env/ios/main.cljs
@@ -0,0 +1,6 @@
+ (ns env.ios.main
+  (:require [precos-mobile.ios.core :as core]))
+
+ (core/init)
+
+
